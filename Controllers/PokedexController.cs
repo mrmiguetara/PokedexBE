efbb858fe5e37e09c806ad2606a0a188c1a179c1
mrmiguetara <miguelag1999@hotmail.com>
2020-02-21 17:46:31 -0400

    Finished API

---
+++ Controllers/PokedexController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+using Newtonsoft.Json;
+using PokedexBE.Models;
+
+namespace PokedexBE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PokedexController : ControllerBase
+    {
+        string baseUrl = "https://pokeapi.co/api/v2";
+        private IHostingEnvironment _hostingEnvironment;
+        public PokedexController(IHostingEnvironment environment)
+        {
+            _hostingEnvironment = environment;
+        }
+
+        // GET: api/Pokedex
+        [HttpGet]
+        public async Task<PokemonItem[]> Get()
+        {
+            string url = $"{baseUrl}/pokemon?limit=150";
+            PokemonResponse pokemonResponse;
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync(url))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    pokemonResponse = JsonConvert.DeserializeObject<PokemonResponse>(apiResponse);
+                }
+            }
+            return pokemonResponse.PokemonItems;
+        }
+
+        // GET: api/Pokedex/5
+        [HttpGet("{id}", Name = "Get")]
+        public async Task<Pokemon> Get(int id)
+        {
+            string url = $"{baseUrl}/pokemon/{id}";
+            Pokemon pokemonResponse;
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync(url))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    pokemonResponse = JsonConvert.DeserializeObject<Pokemon>(apiResponse);
+                }
+            }
+            return pokemonResponse;
+        }
+        private string GetContentType(string path)
+        {
+            var provider = new FileExtensionContentTypeProvider();
+            string contentType;
+            if (!provider.TryGetContentType(path, out contentType))
+            {
+                contentType = "application/octet-stream";
+            }
+            return contentType;
+        }
+        [HttpGet]
+        [Route("download")]
+        public async Task<IActionResult> Download([FromQuery] string pokemonName)
+        {
+            var uploads = Path.Combine(_hostingEnvironment.ContentRootPath, "uploads");
+            if(!System.IO.Directory.Exists(uploads))
+            {
+                System.IO.Directory.CreateDirectory(uploads);
+            }
+            var filePath = Path.Combine(uploads, $"{pokemonName}.txt");
+
+            string url = $"{baseUrl}/pokemon/{pokemonName}";
+            Pokemon pokemonResponse;
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync(url))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    pokemonResponse = JsonConvert.DeserializeObject<Pokemon>(apiResponse);
+                }
+            }
+
+            System.IO.File.WriteAllText(filePath, $"Pokemon name: {pokemonResponse.Name}");
+
+            var memory = new MemoryStream();
+            using (var stream = new FileStream(filePath, FileMode.Open))
+            {
+                await stream.CopyToAsync(memory);
+            }
+            memory.Position = 0;
+
+            return File(memory, GetContentType(filePath), $"{pokemonName}.txt");
+        }
+
+        // POST: api/Pokedex
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Pokedex/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Models/PokemonItem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Globalization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace PokedexBE.Models
+{
+
+
+    public partial class PokemonResponse
+    {
+        [JsonProperty("count")]
+        public long Count { get; set; }
+
+        [JsonProperty("next")]
+        public Uri Next { get; set; }
+
+        [JsonProperty("previous")]
+        public object Previous { get; set; }
+
+        [JsonProperty("results")]
+        public PokemonItem[] PokemonItems { get; set; }
+    }
+
+    public partial class PokemonItem
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("url")]
+        public Uri Url { get; set; }
+    }
+}
